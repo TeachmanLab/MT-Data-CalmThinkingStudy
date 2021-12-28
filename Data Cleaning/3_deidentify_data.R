3810e1ac6fb126342df28b69a691c178d34697fe
Jeremy Eberle <jwe4ec@virginia.edu>
2021-12-27 22:01:49 -0500

    Redact columns in other tables

--- Data Cleaning/3_deidentify_data.R
+++ Data Cleaning/3_deidentify_data.R
@@ -7,6 +7,11 @@
 # Notes ----
 # ---------------------------------------------------------------------------- #
 
+# This script ensures that the raw data are deidentified so that subsequent 
+# cleaning scripts can be run on a deidentified dataset that can be shared and 
+# made public. This way all data cleaning steps can be transparent and 
+# reproducible without compromising identifiable information.
+
 # Before running this script, set your working directory and put the raw data
 # obtained from "1_get_raw_data.ipynb" in a folder called "data/raw". The 
 # script will import the raw data from that folder and, for files that require
@@ -51,52 +56,74 @@ groundhog_day <- version_control()
 raw_data_dir <- paste0(wd_dir, "/data/raw")
 filenames <- list.files(raw_data_dir, pattern = "*.csv", full.names = FALSE)
 
-# Import data files and store them in a list
+# Import data files into list
 
 dat <- lapply(paste0(raw_data_dir, "/", filenames), read.csv)
 
-# Name each data file in the list
+# Name each data table in list
 
 split_char <- "-"
-names(dat) <- unlist(lapply(filenames, 
-                             function(f) {
-                               unlist(strsplit(f, 
-                                               split = split_char, 
-                                               fixed = FALSE))[1]
-                             }
-))
+names(dat) <- unlist(lapply(filenames,
+                            function(f) {
+                              unlist(strsplit(f, 
+                                              split = split_char, 
+                                              fixed = FALSE))[1]
+                            }
+                            ))
 
-# Report the names of the imported tables
+# Report names of imported tables
 
 cat("Imported tables: ")
 names(dat)
 
 # ---------------------------------------------------------------------------- #
-# Deidentify Data ----
+# Specify columns to retain ----
 # ---------------------------------------------------------------------------- #
 
-# Before doing any data cleaning, confirm that the raw data are deidentified so
-# that subsequent cleaning scripts can be run on a deidentified dataset that can
-# be shared and made public. This way all data cleaning steps can be transparent
-# and reproducible without compromising identifiable information.
+# Regarding date-related columns across tables, Bethany Teachman indicated on 
+# 1/13/2021 that MindTrails is not subject to HIPAA regulations, so restrictions 
+# on disclosing dates of training, dates of test measures, and birth years for 
+# any participants over age 89 do not apply
+
+# Regarding "user_agent" in "task_log" table, Dan Funk said on 1/13/2021 that it 
+# is not identifiable. Retain it.
+
+# Regarding "session_id" in "dass21_as" and "oa" tables, Dan Funk said on 1/13/2021
+# that it is transient, encrypted by HTTPS, and used to index participants before 
+# they create an account and get assigned a "participant_id". Retain it.
+
+# "worth_per_week" of "assessing_program" table has what appear to be some unclear
+# responses that will need to addressed in analysis-specific data cleaning, but
+# otherwise the responses are roughly numeric. Retain them.
+
+table(dat$assessing_program$worth_per_week)
+
+# "news" and "social_media" of "covid19" table have what appear to be some
+# unclear responses that will need to be addressed in analysis-specific data
+# cleaning, but otherwise the responses are roughly numeric. Retain them.
+
+table(dat$covid19$news)
+table(dat$covid19$social_media)
 
 # ---------------------------------------------------------------------------- #
-# Determine which columns contain identifiers ----
+# Determine "button_pressed" data in "angular_training" table to redact ----
 # ---------------------------------------------------------------------------- #
 
-# Regarding various dates, Bethany indicated on 1/13/2021 that MindTrails is not
-# subject to HIPAA regulations, so restrictions on dislcosing dates of service, 
-# dates of test measures, and birth years for people over age 89 do not apply.
+# "button_pressed" column of "angular_training" table has some free-text responses. 
+# It has no identifiers for rows with "trial_type" values listed below. However, we 
+# will redact "button_pressed" for rows in which "trial_type" is "FillInBlank", 
+# which contain various kinds of free-text responses (described below).
 
-# TODO: Manually inspect columns and determine which ones may have identifiers
+checked_trial_type_rows <- c("Countdown", "Intro", "MissingLetter",
+                             "page", "Question", "RandomNonPreviousStatement",
+                             "Slider", "ThoughtBubble")
 
-# Consider "button_pressed" column of "angular_training" for rows in which
-# "trial_type" is "FillInBlank". No identifiers are present for rows in which
-# "trial_type" is "Countdown", "Intro", "MissingLetter", "page", "Question",
-# "RandomNonPreviousStatement", "Slider", or "ThoughtBubble".
+table(dat$angular_training$trial_type[!(dat$angular_training$trial_type %in% 
+                                          checked_trial_type_rows)],
+      useNA = "always")
 
-# 1. Some rows are participant descriptions of an anxious situation for the Use 
-# Your Imagination task at "firstSession" before starting training
+#   1. Some rows are participant descriptions of an anxious situation for the Use 
+#   Your Imagination task at "firstSession" before starting training
 
 rows1 <- dat$angular_training[dat$angular_training$trial_type == 
                                 "FillInBlank" &
@@ -105,19 +132,19 @@ rows1 <- dat$angular_training[dat$angular_training$trial_type ==
                                    dat$angular_training$step_title == 
                                    "Use your Imagination"), ]
 
-# Note that issues regarding entries (a) for unexpected values of "conditioning" 
-# and (b) at sessions other than "firstSession" will be addressed in later data 
-# cleaning. For now, we focus on deidentification.
+#   Note: Analysis-specific data cleaning will need to address issues with entries
+#   (a) for unexpected values of "conditioning" and (b) at sessions other than 
+#   "firstSession". For now, we focus on deidentification.
 
 table(rows1$conditioning)
 table(rows1$session)
 
-# 2. Some rows are participant responses for training scenarios at "fifthSession"
-# that required filling in a blank (vs. completing a word fragment). Prior to
-# 2/15/2019, these responses were indexed not with a "step_title" value of 
-# "scenario", but with a "step_title" value of the scenario's title, which 
-# subsequently was stored in "stimulus_name". Among the scenario titles prior 
-# to this change was "pub"; after this change, "pub" was renamed to "bar".
+#   2. Some rows are participant responses for training scenarios at "fifthSession"
+#   that required filling in a blank (vs. completing a word fragment). Prior to
+#   2/15/2019, these responses were indexed not with a "step_title" value of 
+#   "scenario", but with a "step_title" value of the scenario's title, which 
+#   subsequently was stored in "stimulus_name". Among the scenario titles prior 
+#   to this change was "pub"; after this change, "pub" was renamed to "bar".
 
 scenario_titles <- 
   c(unique(dat$angular_training[dat$angular_training$step_title == 
@@ -128,19 +155,19 @@ rows2 <- dat$angular_training[dat$angular_training$trial_type == "FillInBlank" &
                                    dat$angular_training$step_title %in% 
                                    scenario_titles), ]
 
-# 3. Henry Behan said that this criterion reflects participants' responses to 
-# the Quick Thinking Exercise (also called Flexible Thinking Exercise). 
+#   3. Henry Behan said that this criterion reflects participants' responses to 
+#   the Quick Thinking Exercise (also called Flexible Thinking Exercise)
 
 rows3 <- dat$angular_training[dat$angular_training$stimulus_name == 
                                 "flex_thinking_explanations", ]
 
 table(rows3$trial_type)
 
-# Note that later data cleaning will address these issues: (a) not all rows have 
-# "step_title" of "Exercise: Quick Thinking" due to a programming error, (b) some 
-# Calm Thinking participants appear to have completed this task, and (c) for 
-# "CONTROL" participants, "session" is populated by "flexible_thinking", so the
-# session number is unclear.
+#   Note: Analysis-specific data cleaning will need to address these issues: (a) 
+#   not all rows have "step_title" of "Exercise: Quick Thinking" due to programming 
+#   error; (b) some Calm Thinking participants appear to have completed this task; 
+#   and (c) for "CONTROL" participants, "session" is populated by "flexible_thinking", 
+#   so the session number is unclear.
 
 table(rows3$step_title)
 
@@ -149,9 +176,9 @@ table(rows3$conditioning)
 table(rows3$session)
 table(rows3[rows3$session == "flexible_thinking", ]$conditioning)
 
-# 4. Henry Behan said these criteria reflect scenarios created by participants 
-# in the Write Your Own Scenario exercise in the "TRAINING_CREATE" condition of 
-# the TET study.
+#   4. Henry Behan said these criteria reflect scenarios created by participants 
+#   in the Write Your Own Scenario exercise in the "TRAINING_CREATE" condition of 
+#   the TET study.
 
 rows4_training_create <- dat$angular_training[dat$angular_training$trial_type == 
                                                 "FillInBlank" &
@@ -162,8 +189,8 @@ rows4_training_create <- dat$angular_training[dat$angular_training$trial_type ==
                                                 dat$angular_training$step_title == 
                                                   "", ]
 
-# However, one admin/test account in "CONTROL" also appears to have completed 
-# this task, so we cannot restrict to "TRAINING_CREATE" for deidentification.
+#   However, one admin/test account in "CONTROL" also appears to have completed 
+#   this task, so we cannot restrict to "TRAINING_CREATE" for deidentification
 
 rows4_all_conditions <- dat$angular_training[dat$angular_training$trial_type == 
                                                "FillInBlank" &
@@ -174,9 +201,9 @@ rows4_all_conditions <- dat$angular_training[dat$angular_training$trial_type ==
 
 table(rows4_all_conditions$conditioning)
 
-# 5. Henry Behan said this criterion reflects participants' explanations as to 
-# why the they created occurred in the Write Your Own Scenario exercise in the
-# "TRAINING_CREATE" condition of the TET study
+#   5. Henry Behan said this criterion reflects participants' explanations as to 
+#   why the scenario they created occurred in the Write Your Own Scenario exercise 
+#   in the "TRAINING_CREATE" condition of the TET study
 
 rows5 <- dat$angular_training[dat$angular_training$stimulus_name == 
                                 "training_create_explanations", ]
@@ -193,79 +220,116 @@ remaining <- dat$angular_training[!(dat$angular_training$id %in% ignored_ids) &
 
 nrow(remaining) == 0
 
+# ---------------------------------------------------------------------------- #
+# Redact certain "button_pressed" entries in "angular_training" ----
+# ---------------------------------------------------------------------------- #
 
-
-
-
-# Consider "compare_to_others" and "worth_per_week" of "assessing_program"
-
-# Consider "difficult_to_understand", "other_feedback", and 
-# "technical_difficulties" of "coach_prompt"
-
-# Consider "news" and "social_media" of "covid19"
-
-# Consider "ptp_reason_other" in "demographics"
-
-# Consider "other_place", "other_reason_control", "problems_desc",
-# "other_coaching", and "other_help_topic" in "evaluation"
-
-# Consider "other" in "help_seeking"
-
-# Consider "change_help_text", "help_other_text", "other_disorder", and
-# "other_why_no_help" in "mental_health_history"
-
-# Consider "end_other_desc", "change_med_desc", "control_desc", and 
-# "location_desc" in "reasons_for_ending"
-
-# Consider "not_return_reasons" in "return_intention"
-
-# Consider "other_distraction" and "other_location_desc" in "session_review"
+# TODO
 
 
 
 
 
-# Regarding "user_agent" in "task_log", Dan Funk said on 1/13/2021 that it is 
-# not identifiable. Retain it.
-
-# Regarding "session_id" in "dass21_as" and "oa", Dan Funk said on 1/13/2021
-# that it is transient, encrypted by HTTPS, and used to index participants
-# before they create an account and get assigned a "participant_id". Retain it.
-
-
-
-
-
-# TODO: Asked Dan for report of free-response columns on 1/11/20.
-
-
-
-
-
-# TODO: Redact "order_id" in "gift_log" (and in "error" column of "import_log")
-# for security reasons
-
-
-
-
-
-# TODO: Redact "email", "full_name", "password", and "phone" in "participant" 
-# for admin and test accounts that have this data.
-
-View(dat$participant[dat$participant$email != "" |
-                       dat$participant$full_name != "" |
-                       dat$participant$password != "" |
-                       !is.na(dat$participant$phone), ])
 
 
 
 
 
 # ---------------------------------------------------------------------------- #
-# Remove phone numbers from "sms_log" ----
+# Redact columns in other tables ----
 # ---------------------------------------------------------------------------- #
 
-# Remove phone numbers from "exception" column of "sms_log"
+# Specify a character vector of columns ("<table_name>$<column_name>") whose 
+# values should be replaced with "REDACTED_BY_CLEANING_SCRIPT". If no column is 
+# to be redacted, specify NULL without quotes (i.e., "redact_columns <- NULL").
+
+# The following columns should be redacted because they have free-text responses
+# that are currently too time consuming to manually check for deidentification
+
+redact_free_text_cols <- c("assessing_program$compare_to_others",
+                           "coach_prompt$difficult_to_understand",
+                           "coach_prompt$other_feedback",
+                           "coach_prompt$technical_difficulties",
+                           "demographics$ptp_reason_other",
+                           "evaluation$other_place",
+                           "evaluation$other_reason_control",
+                           "evaluation$problems_desc",
+                           "evaluation$other_coaching",
+                           "evaluation$other_help_topic",
+                           "help_seeking$other",
+                           "mental_health_history$change_help_text",
+                           "mental_health_history$help_other_text",
+                           "mental_health_history$other_disorder",
+                           "mental_health_history$other_why_no_help",
+                           "reasons_for_ending$end_other_desc",
+                           "reasons_for_ending$change_med_desc",
+                           "reasons_for_ending$control_desc",
+                           "reasons_for_ending$location_desc",
+                           "return_intention$not_return_reasons",
+                           "session_review$other_distraction",
+                           "session_review$other_location_desc")
+
+# Redact "email", "full_name", "password", and "phone" in "participant" table
+# for admin and test accounts that have this data
+
+redact_participant_cols <- c("participant$email", "participant$full_name",
+                             "participant$password", "participant$phone")
+
+# Collect all columns to redact
+
+redact_cols <- c(redact_free_text_cols, redact_participant_cols)
+
+# Define function to recode any meaningful values in given columns to be redacted
+# as "REDACTED_BY_CLEANING_SCRIPT". Regarding meaningful values, note that in some
+# columns "N/A" appears to be a programmed response option, whereas in others it
+# does not (also, users could still manually type some variation of "N/A"); thus, 
+# cases of "N/A" should not be interpreted as exhaustive of the category.
+
+redact_columns <- function(dat, redact_cols) {
+  output <- vector("list", length(dat))
+  
+  for (i in 1:length(dat)) {
+    output[[i]] <- dat[[i]]
+    
+    for (j in 1:length(dat[[i]])) {
+      table_i_name <- names(dat[i])
+      column_j_name <- names(dat[[i]][j])
+      table_i_column_j_name <- paste0(table_i_name, "$", column_j_name)
+      
+      if (table_i_column_j_name %in% redact_cols) {
+        output[[i]][!(output[[i]][, column_j_name] %in% c("", "N/A", "555")) &
+                      !(is.na(output[[i]][, column_j_name])), 
+                    column_j_name] <- "REDACTED_BY_CLEANING_SCRIPT"
+      }
+    }
+  }
+  
+  names(output) <- names(dat)
+  return(output)
+}
+
+# Run function
+
+dat <- redact_columns(dat, redact_cols)
+
+# ---------------------------------------------------------------------------- #
+# TODO: Redact "order_id" data from "gift_log" and "import_log" ----
+# ---------------------------------------------------------------------------- #
+
+# Redact "order_id" in "gift_log" table (and in "error" column of "import_log"
+# table) for security reasons
+
+View(dat$import_log)
+
+
+
+
+
+# ---------------------------------------------------------------------------- #
+# Redact phone numbers from "sms_log" ----
+# ---------------------------------------------------------------------------- #
+
+# Redact phone numbers from "exception" column of "sms_log"
 
 ignored_values <- c("A 'To' phone number is required.",
                     "Authenticate",
@@ -278,23 +342,23 @@ View(temp)
 
 dat$sms_log[grepl("Permission to send an SMS has not been enabled for the region indicated by the 'To' number:", 
                   dat$sms_log$exception), ]$exception <-
-  "Permission to send an SMS has not been enabled for the region indicated by the 'To' number: [REDACTED]"
+  "Permission to send an SMS has not been enabled for the region indicated by the 'To' number: [REDACTED_BY_CLEANING_SCRIPT]"
 
 dat$sms_log[grepl("The 'To' number", 
                   dat$sms_log$exception) &
                grepl("is not a valid phone number.", 
                      dat$sms_log$exception), ]$exception <-
-  "The 'To' number [REDACTED] is not a valid phone number."
+  "The 'To' number [REDACTED_BY_CLEANING_SCRIPT] is not a valid phone number."
 
 dat$sms_log[grepl("To number", dat$sms_log$exception) &
                grepl("is not a mobile number", 
                      dat$sms_log$exception), ]$exception <- 
-  "To number: [REDACTED], is not a mobile number"
+  "To number: [REDACTED_BY_CLEANING_SCRIPT], is not a mobile number"
 
 deidentified_values <- 
-  c("Permission to send an SMS has not been enabled for the region indicated by the 'To' number: [REDACTED]",
-    "The 'To' number [REDACTED] is not a valid phone number.",
-    "To number: [REDACTED], is not a mobile number")
+  c("Permission to send an SMS has not been enabled for the region indicated by the 'To' number: [REDACTED_BY_CLEANING_SCRIPT]",
+    "The 'To' number [REDACTED_BY_CLEANING_SCRIPT] is not a valid phone number.",
+    "To number: [REDACTED_BY_CLEANING_SCRIPT], is not a mobile number")
 
 temp2 <- dat$sms_log[dat$sms_log$exception != "" &
                        !(dat$sms_log$exception %in% ignored_values) &
